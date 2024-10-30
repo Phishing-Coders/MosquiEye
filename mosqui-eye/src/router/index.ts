8f96ddb48d1bb325a77c45332ff7176b0f653b4f
Nepz-dev <gamingrig77@gmail.com>
2024-10-30 14:15:39 +0800

    Redirect Page
    Updated Version Router

--- mosqui-eye/src/router/index.ts
+++ mosqui-eye/src/router/index.ts
@@ -3,13 +3,18 @@ import { RouteRecordRaw } from 'vue-router';
 
 const routes: Array<RouteRecordRaw> = [
   {
+    path: '/',
+    redirect: '/LoginPage'
+  },
+  {
     path: '/LoginPage',
     component: () => import('@/views/LoginPage.vue')
-  }]
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
-})
+});
 
-export default router
+export default router;
